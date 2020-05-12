506b4c17c4ec6a2c8b18461abb7aacf8cbc24052
Matt Im <matthew.seiim@gmail.com>
2020-05-12 14:26:59 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+class Artist 
+
+attr_accessor :name, :song
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+
+    
+  def songs 
+    Song.all.select do |song|
+      song.artist == self 
+    end
+  end
+  
+  def find_or_create_by_name(name)
+    if self.artist.nil?
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song 
+
+attr_accessor :name, :artist
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all 
+    @@all
+  end
+
+#   def self.new_by_filename(filename)
+#     self.split(" - ") = Song.new[1]
+#     @artist << self 
+#   end
+#   self
+# end
+    
+
+
+end
